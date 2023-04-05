48edf4d02d4db41a71336d9265de7e210d5486bd
Lashanique <lcars34@wgu.edu>
2023-04-04 21:18:49 -0400

    math

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = 100;
+Number.isInteger(random);
+
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(Math.mod);
+
+let max = (5,10,15,20);
+console.log(Math.max(max));
